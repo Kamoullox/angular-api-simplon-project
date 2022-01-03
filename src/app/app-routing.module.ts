e211d9bb0f6e990995f01d1dd3c717106e02142d
Kamoullox <boucetta.kamel62@gmail.com>
2022-01-03 11:09:10 +0100

    fix detail page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { AppComponent } from './app.component';
 import { DetailBookComponent } from './detail-book/detail-book.component';
 import { HomeComponent } from './home/home.component';
-import { BooksLibraryComponent } from './books-library/books-library.component';
 
 const routes: Routes = [
   {path: '', component: HomeComponent},
