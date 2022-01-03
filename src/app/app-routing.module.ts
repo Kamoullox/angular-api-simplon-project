f423b0eb4bb3b13e16a1789029e34423ac721770
yohan-lertourne <yohan.lertourne@gmail.com>
2022-01-03 10:50:47 +0100

    Update app-routing.module.ts

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,6 +3,7 @@ import { RouterModule, Routes } from '@angular/router';
 import { AppComponent } from './app.component';
 import { DetailBookComponent } from './detail-book/detail-book.component';
 import { HomeComponent } from './home/home.component';
+import { BooksLibraryComponent } from './books-library/books-library.component';
 
 const routes: Routes = [
   {path: '', component: HomeComponent},
